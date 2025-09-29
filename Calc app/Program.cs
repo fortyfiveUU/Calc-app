143305357d5a24daf53ee2e3404ef8024f70eca2
fortyfiveuu <hamilton-w6@ulster.ac.uk>
2025-09-29 16:35:34 +0100

    Added operations

--- Calc app/Program.cs
+++ Calc app/Program.cs
@@ -1,15 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿CalculatorApp();
 
+void CalculatorApp()
+{
+    //clare variables and initialize them to zero
+    int firstnumber = 0;
+    int secondnumber = 0;
+    int result = 0;
 
-Console.WriteLine("enter the first Number");
+    // prompt user to enter the first number
+    Console.WriteLine("enter the first Number");
+    firstnumber = Convert.ToInt32(Console.ReadLine());
+    // prompt user to enter the second number
+    Console.WriteLine("enter the second number");
 
+    secondnumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("choose an option from the following: ");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Multiply");
+    Console.WriteLine("4 - Divide");
 
-int firstnumber = Convert.ToInt32(Console.ReadLine());
+    //perform calculation
+    result = firstnumber + secondnumber;
+    Console.WriteLine();
+    Console.WriteLine("The addition of both numbers is {0}", result);
 
-Console.WriteLine("enter the second number");
-
-int secondnumber = Convert.ToInt32(Console.ReadLine());
-
-int result = firstnumber + secondnumber;
-
-Console.WriteLine("The addition of both numbers is {0}", result);
+    Console.ReadLine();
+}
