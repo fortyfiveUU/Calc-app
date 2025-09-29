c41d102ecdd4a1b86571f14a6a03b8b5ac012f62
fortyfiveuu <hamilton-w6@ulster.ac.uk>
2025-09-29 16:45:43 +0100

    made the operations work

--- Calc app/Program.cs
+++ Calc app/Program.cs
@@ -6,6 +6,7 @@ void CalculatorApp()
     int firstnumber = 0;
     int secondnumber = 0;
     int result = 0;
+    int choice = 0;
 
     // prompt user to enter the first number
     Console.WriteLine("enter the first Number");
@@ -19,11 +20,45 @@ void CalculatorApp()
     Console.WriteLine("2 - Subtract");
     Console.WriteLine("3 - Multiply");
     Console.WriteLine("4 - Divide");
+    //Convert string to int
+    choice = Convert.ToInt32(Console.ReadLine());
+    // use if statement to perform selected operation
+    if (choice == 1)
+    {
+        result = firstnumber + secondnumber;
+        Console.WriteLine("The addition of both numbers is {0}", result);
+    }
+    else if (choice == 2)
+    {
+        result = firstnumber - secondnumber;
+        Console.WriteLine("The subtraction of both numbers is {0}", result);
+    }
+    else if (choice == 3)
+    {
+        result = firstnumber * secondnumber;
+        Console.WriteLine("The multiplication of both numbers is {0}", result);
+    }
+    else if (choice == 4)
+    {
+        if (secondnumber != 0)
+        {
+            result = firstnumber / secondnumber;
+            Console.WriteLine("The division of both numbers is {0}", result);
+        }
+        else
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice");
 
-    //perform calculation
-    result = firstnumber + secondnumber;
-    Console.WriteLine();
-    Console.WriteLine("The addition of both numbers is {0}", result);
+        //perform calculation
 
-    Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("The addition of both numbers is {0}", result);
+
+        Console.ReadLine();
+    }
 }
