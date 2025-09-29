bb9542e0e4bf229f8e468006faf73d0aef68913f
fortyfiveuu <hamilton-w6@ulster.ac.uk>
2025-09-29 16:01:22 +0100

    initial commit of software

--- Calc app/Program.cs
+++ Calc app/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("First Number");
+
+
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Second Number");
+
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstnumber + secondnumber;
+
+Console.WriteLine("The addition of both numbers is {0}", result);
